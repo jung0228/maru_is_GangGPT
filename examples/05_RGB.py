6c506b67c11f7239e849abe92955294b9c0d3f0d
Devin <support@adeept.com>
2024-04-23 17:16:29 +0800

    Update

--- examples/05_RGB.py
+++ examples/05_RGB.py
@@ -2,25 +2,27 @@
 from gpiozero import PWMOutputDevice as PWM
 import time
 
-R1 = 8
-G1 = 7
-B1 = 0
+Left_R = 8
+Left_G = 7
+Left_B = 0
 
-R2 = 1
-G2 = 5
-B2 = 6
+Right_R = 1
+Right_G = 5
+Right_B = 6
 
 colors = [0xFF0000, 0x00FF00, 0x0000FF, 0xFFFF00, 0xFF00FF, 0x00FFFF, 0X6F00D2, 0xFF5809]
 # colors = [0xFF0000,0x00FF00,0x00FF00]
 
-def setup(Rpin, Gpin, Bpin):
-  global pins
-  global p_R, p_G, p_B
-  pins = {'pin_R': Rpin, 'pin_G': Gpin, 'pin_B': Bpin}
+def setup():
+  global L_R, L_G, L_B, R_R, R_G, R_B
 
-  p_R = PWM(pin=pins['pin_R'],initial_value=1.0, frequency=2000)
-  p_G = PWM(pin=pins['pin_G'],initial_value=1.0, frequency=2000)
-  p_B = PWM(pin=pins['pin_B'],initial_value=1.0, frequency=2000)
+  L_R = PWM(pin=Left_R, initial_value=1.0, frequency=2000)
+  L_G = PWM(pin=Left_G, initial_value=1.0, frequency=2000)
+  L_B = PWM(pin=Left_B, initial_value=1.0, frequency=2000)
+
+  R_R = PWM(pin=Right_R, initial_value=1.0, frequency=2000)
+  R_G = PWM(pin=Right_G, initial_value=1.0, frequency=2000)
+  R_B = PWM(pin=Right_B, initial_value=1.0, frequency=2000)
 
 
 
@@ -28,7 +30,7 @@ def map(x, in_min, in_max, out_min, out_max):
   return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min
 
 
-def setColor(col):   # For example : col = 0x112233
+def setAllColor(col):   # For example : col = 0x112233
   R_val = (col & 0xff0000) >> 16
   G_val = (col & 0x00ff00) >> 8
   B_val = (col & 0x0000ff) >> 0
@@ -37,36 +39,46 @@ def setColor(col):   # For example : col = 0x112233
   G_val = map(G_val, 0, 255, 0, 1.00)
   B_val = map(B_val, 0, 255, 0, 1.00)
   
-  p_R.value = 1.0-R_val
-  p_G.value = 1.0-G_val
-  p_B.value = 1.0-B_val
+  L_R.value = 1.0-R_val
+  L_G.value = 1.0-G_val
+  L_B.value = 1.0-B_val
 
-def setRGBColor(R,G,B):   # For example : col = 0x112233
+  R_R.value = 1.0-R_val
+  R_G.value = 1.0-G_val
+  R_B.value = 1.0-B_val
+
+def setAllRGBColor(R,G,B):   # For example : col = 0x112233
 
   R_val = map(R, 0, 255, 0, 1.00)
   G_val = map(G, 0, 255, 0, 1.00)
   B_val = map(B, 0, 255, 0, 1.00)
   
-  p_R.value = 1.0-R_val
-  p_G.value = 1.0-G_val
-  p_B.value = 1.0-B_val
+  L_R.value = 1.0-R_val
+  L_G.value = 1.0-G_val
+  L_B.value = 1.0-B_val
+
+  R_R.value = 1.0-R_val
+  R_G.value = 1.0-G_val
+  R_B.value = 1.0-B_val
 
 def loop():
   while True:
     for col in colors:
-      setColor(col)
+      setAllColor(col)
       time.sleep(0.5)
 
 def destroy():
-  p_R.stop()
-  p_G.stop()
-  p_B.stop()
+  L_R.stop()
+  L_G.stop()
+  L_B.stop()
+  R_R.stop()
+  R_G.stop()
+  R_B.stop()
 
 if __name__ == "__main__":
 
+  setup()
   try:
-    #setup(R2, G2, B2)
-    setup(R1, G1, B1)
     loop()
   except KeyboardInterrupt:
     destroy()
